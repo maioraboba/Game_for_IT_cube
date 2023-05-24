f9111e3ac0b8bf40e9a69cf74e1dfe5015b1d77b
maioraboba <mr.pechenka2007@mail.ru>
2023-05-24 18:46:09 +1000

    siuuu

--- Game_IT_cube.py
+++ Game_IT_cube.py
@@ -15,6 +15,7 @@ clock = pygame.time.Clock()
 
 # загрузка музыки
 pygame.mixer.music.load("Lines of Code.mp3")
+my_sound = pygame.mixer.music
 
 font = pygame.font.SysFont('Verdana.ttf', 20, True)
 big_font = pygame.font.SysFont('Verdana.ttf', 24, True)
@@ -46,15 +47,19 @@ class MainMenu(pygame_menu.Menu):
     def settings_menu(self):
         self._open(self.settings)
 
+    def take_text_value(self, name):
+        Player.name = name
+
     def __init__(self):
-        super().__init__('Welcome', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_SOLARIZED)
-        self.add.text_input('Name: ', default='username', maxchar=20)
+        super().__init__('Welcome to Bubble Fight', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_DARK)
+        self.level_volume = 10
+        self.add.text_input('Name: ', default='username', maxchar=20, onchange=set_difficulty)
         self.add.button('Play', self.start_the_game)
         self.add.button('Settings', self.settings_menu)
         self.add.button('Quit', pygame_menu.events.EXIT)
 
-        self.settings = pygame_menu.Menu('Select a Difficulty', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_BLUE)
-        self.settings.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=self.set_difficulty)
+        self.settings = pygame_menu.Menu('Settings :', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_DARK)
+        self.settings.add.progress_bar('Volume :', self.level_volume)
 
 
 # class PauseMenu(MainMenu):
@@ -86,7 +91,7 @@ class Game:
     def start(self):
         global camera
         self.is_running = True
-        pygame.mixer.music.pause()
+        my_sound.pause()
         while self.is_running:
             clock.tick(FPS)
             eventts = pygame.event.get()
@@ -197,15 +202,15 @@ class Grid(CanPaint):
 class Player(CanPaint):
     FONT_COLOR = "#2a2a2c"
 
-    def __init__(self, surface, cam, name=""):
+    def __init__(self, surface, cam):
         super().__init__(surface, cam)
+        self.name = ""
         self.x, self.y = rnd.randint(300, 2200), rnd.randint(300, 2200)
         self.mass, self.speed = 20, 4
+        self.outline_size = 3 + self.mass / 2
         self.color = "#1c5bed"
         self.outline_color = "#ccccff"
-        if name:
-            self.name = name
-        else:
+        if not self.name:
             self.name = "barabashka"
 
     def collision_check(self, foods):
@@ -226,17 +231,17 @@ class Player(CanPaint):
         else:
             vy = self.speed - abs(vx)
 
-        if self.x + vx - self.mass / 2 < 0:
-            self.x = self.mass / 2
-        elif self.x + vx + self.mass / 2 > WIDTH_ZONE:
-            self.x = WIDTH_ZONE - self.mass / 2
+        if self.x + vx - self.outline_size < 0:
+            self.x = self.mass / 2 + self.outline_size
+        elif self.x + vx + self.outline_size > WIDTH_ZONE:
+            self.x = WIDTH_ZONE - self.outline_size
         else:
             self.x += vx
 
-        if self.y + vy - self.mass / 2 < 0:
-            self.y = self.mass / 2
-        elif self.y + vy + self.mass / 2 > HEIGHT_ZONE:
-            self.y = HEIGHT_ZONE - self.mass / 2
+        if self.y + vy - self.outline_size < 0:
+            self.y = self.outline_size
+        elif self.y + vy + self.outline_size > HEIGHT_ZONE:
+            self.y = HEIGHT_ZONE - self.outline_size
         else:
             self.y += vy
 
@@ -272,7 +277,11 @@ camera = Camera()
 
 grid = Grid(screen, camera)
 cells = CellList(screen, camera, 1000)
-bacterium = Player(screen, camera, "Bebrochka")
+bacterium = Player(screen, camera)
+
+mainmenu = MainMenu()
+my_sound.play(-1)
+my_sound.set_volume(0.1)
 
 painter = Painter()
 painter.add(grid)
@@ -281,8 +290,7 @@ painter.add(bacterium)
 
 
 # pausemenu = PauseMenu()
-mainmenu = MainMenu()
-pygame.mixer.music.play(-1)
+
 while True:
     events = pygame.event.get()
     for event in events:
